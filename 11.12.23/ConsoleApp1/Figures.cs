3ec7d4e0a6cb0195a0b7e92e74540ab359207278
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2023-12-11 15:58:19 +0200

    Add files via upload

---
+++ 11.12.23/ConsoleApp1/Figures.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Класс описывающий фигуру
+    /// </summary>
+    class Figures
+    {
+
+
+        /// <summary>
+        /// создание поле имени
+        /// </summary>
+        public string name;
+
+        /// <summary>
+        /// создание поле стороны a
+        /// </summary>
+        public int side1;
+
+        /// <summary>
+        /// создание поле стороны b
+        /// </summary>
+        public int side2;
+
+        /// <summary>
+        /// создание поле стороны c
+        /// </summary>
+        public int side3;
+
+        /// <summary>
+        /// Конструктор по умолчанию(без параметров)
+        /// </summary>
+        public Figures()
+        {
+        }
+
+        /// <summary>
+        /// Конструктор с параметрами 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="side1"></param>
+        /// <param name="side2"></param>
+        public Figures (string name, int side1, int side2)
+        {
+            this.name = name;
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        /// <summary>
+        /// Конструктор с параметрами 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="side1"></param>
+        /// <param name="side2"></param>
+        /// <param name="side3"></param>
+        public Figures(string name, int side1, int side2,int side3):this(name,side1,side2)
+        {
+            this.side3 = side3;
+        }
+
+        /// <summary>
+        /// Метод, который выводит информацию о фигуре 
+        /// </summary>
+        /// <returns></returns>
+        public string ShowInfo()
+        {
+            return $"Фигура - {name}\nСторона a - {side1}\nСторона b - {side2}\nСторона с - {side3}";
+
+        }
+
+        /// <summary>
+        /// Метод, который выводит информацию о периметре и площади квадрата 
+        /// </summary>
+        /// <returns></returns>
+        public string PP()
+        {
+           return $"Периметр квадрата: {side1 * 4}\nПлощадь к" +
+                $"вадрата: {Math.Pow(side1,2)}";
+        }
+
+
+    }
+}
---
+++ 11.12.23/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //1
+        static void Main(string[] args)
+        {
+            
+            Figures figures = new Figures();
+            figures.name = "Квадрат";
+            figures.side1 = 5;
+            figures.side2 = 5;
+            Console.WriteLine(figures.ShowInfo());
+
+            Console.WriteLine(figures.PP());
+
+            Figures figures1 = new Figures();
+            figures.name = "Прямоугольник";
+            figures.side1 = 4;
+            figures.side2 = 8;
+            Console.WriteLine(figures.ShowInfo());
+
+
+            /// <summary>
+            /// Способ, где параметры вводятся с клавиатуры 
+            /// </summary>
+            Console.Write("Фигура: ");
+            string name = Console.ReadLine();
+            Console.Write("Сторона a: ");
+            int side1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сторона b: ");
+            int side2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сторона c: ");
+            int side3 = Convert.ToInt32(Console.ReadLine());
+
+            Figures figures3 = new Figures(name,side1,side2,side3);
+            Console.WriteLine(figures3.ShowInfo());
+
+            Figures figures4 = new Figures("Параллелограмм", 8, 3);
+            Console.WriteLine(figures4.ShowInfo());
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
